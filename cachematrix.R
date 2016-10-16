b6d754104db9066b42e8caa9ba539d2de8b03387
Dossland <geneticdoss@gmail.com>
2016-10-16 10:39:52 -0700

    Declared Inverse and Set Variables

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
 }
 
 
