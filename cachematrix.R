7e96d264ed45cb2cd0bbd2a9ff87e002f67b44bf
Dossland <geneticdoss@gmail.com>
2016-10-16 10:49:13 -0700

    Added getInverse and the creation of a list

--- cachematrix.R
+++ cachematrix.R
@@ -11,6 +11,8 @@ makeCacheMatrix <- function(x = matrix()) {
   
     get <- function() x
     setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
