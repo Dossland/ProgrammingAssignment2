9f0fe7755546221e0a0ae1fe68832a913813088e
Dossland <geneticdoss@gmail.com>
2016-10-16 11:00:24 -0700

    Removed Default Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -16,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
     inv <- x$getinverse()
