73dfabfaddeb6755b41391341dc038ae2e789cb4
Dossland <geneticdoss@gmail.com>
2016-10-16 10:59:59 -0700

    Completed cacheSolve - No errors in R studios

--- cachematrix.R
+++ cachematrix.R
@@ -23,5 +23,9 @@ cacheSolve <- function(x, ...) {
     if(!is.null(inv)) {
          message("getting cached data.")
     return(inv)
-  }
+    }
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinverse(inv)
+    inv
 }
